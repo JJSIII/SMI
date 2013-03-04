e0f60203f423040ef017cd8aa77ea2179b0a608e
Jackson Stephens <filthee@gmail.com>
2013-03-04 16:32:29 -0500

    Menu bar

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,1 +1,3 @@
-$(document).ready(function(){});
+// hide address bar
+//*************************************************************
+(function(e){var t=e.document;if(!location.hash&&e.addEventListener){window.scrollTo(0,1);var n=1,r=function(){return e.pageYOffset||t.compatMode==="CSS1Compat"&&t.documentElement.scrollTop||t.body.scrollTop||0},i=setInterval(function(){if(t.body){clearInterval(i);n=r();e.scrollTo(0,n===1?0:1)}},15);e.addEventListener("load",function(){setTimeout(function(){r()<20&&e.scrollTo(0,n===1?0:1)},0)})}})(this);$(document).ready(function(){});
