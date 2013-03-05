b7d1249c0a976b13dd3ab966252ae61518656482
Jackson Stephens <filthee@gmail.com>
2013-03-05 13:37:00 -0500

    minor

--- js/main.js
+++ js/main.js
@@ -37,6 +37,62 @@
 
 $(document).ready(function() {
 
+	// icon font
+
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-pushpin' : '&#x21;',
+			'icon-windows8' : '&#x22;',
+			'icon-windows' : '&#x23;',
+			'icon-dashboard' : '&#x24;',
+			'icon-flag' : '&#x25;',
+			'icon-star' : '&#x26;',
+			'icon-warning' : '&#x27;',
+			'icon-info' : '&#x28;',
+			'icon-checkmark-circle' : '&#x29;',
+			'icon-cancel-circle' : '&#x2a;',
+			'icon-close' : '&#x2b;',
+			'icon-plus' : '&#x2c;',
+			'icon-ticket' : '&#x2d;',
+			'icon-cog' : '&#x2e;',
+			'icon-cogs' : '&#x2f;',
+			'icon-alarm' : '&#x30;',
+			'icon-list' : '&#x31;',
+			'icon-wrench' : '&#x32;',
+			'icon-user' : '&#x33;',
+			'icon-calendar' : '&#x34;',
+			'icon-arrow-down' : '&#x35;',
+			'icon-arrow-left' : '&#x36;',
+			'icon-arrow-right' : '&#x37;',
+			'icon-arrow-up' : '&#x38;',
+			'icon-checkmark' : '&#x39;',
+			'icon-minus' : '&#x3a;',
+			'icon-tags' : '&#x3b;',
+			'icon-new-tab' : '&#x3c;',
+			'icon-search' : '&#x3d;',
+			'icon-power' : '&#x3e;',
+			'icon-envelop' : '&#x3f;',
+			'icon-phone' : '&#x40;',
+			'icon-tools' : '&#x41;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+
 	// menu-bar tooltips
 	$(".menu-bar a").tipper({
 		direction: "bottom"
